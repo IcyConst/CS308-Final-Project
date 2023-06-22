f2241d704450dbb89e37993966208114063c152e
Sunjar <magic_0715@163.com>
2023-06-23 00:47:24 +0800

    change dataset

--- remove.py
+++ remove.py
@@ -2,12 +2,12 @@ import os
 
 # 遍历所有子文件夹，统计每个子文件夹中的照片数量
 photo_counts = {}
-for dirpath, dirnames, filenames in os.walk("training"):
+for dirpath, dirnames, filenames in os.walk("validation"):
     photo_counts[dirpath] = len(filenames)
 
 # 删除照片数量大于等于 100 的子文件夹
 for dirpath, photo_count in photo_counts.items():
-    if photo_count >= 30:
+    if photo_count >= 14:
         print("Deleting folder:", dirpath)
         for filename in os.listdir(dirpath):
             file_path = os.path.join(dirpath, filename)
